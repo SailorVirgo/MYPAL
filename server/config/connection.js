ec62247fe59c0a9527883992614fffbdb80fdcfc
ricanlonghorn23 <luis.ra.aldaz@gmail.com>
2024-08-01 00:03:21 -0500

    addedenvfile

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
 
-// Use the environment variable for the connection string, or fallback to localhost for local development
-const dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/The-Quiet-Ones';
-
-mongoose.connect(dbUri);
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to ' + dbUri);
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error('Mongoose connection error: ' + err);
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/The-Quiet-Ones');
 
 module.exports = mongoose.connection;
