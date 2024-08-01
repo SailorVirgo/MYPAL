1121c1d7a87fecfbc756cd64d171d12987b50bae
ricanlonghorn23 <luis.ra.aldaz@gmail.com>
2024-07-31 21:52:34 -0500

    edit to ui

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,15 +1,7 @@
-
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import {
-  Header,
-  Icon,
-  HeaderContent,
-  Menu,
-  Container,
-  Dropdown,
-} from "semantic-ui-react";
-import Auth from "../../utils/auth";
+import React , { useState } from 'react'
+import { Header, Icon, HeaderContent, Menu, Container, Dropdown } from 'semantic-ui-react'
+import Auth from '../../utils/auth';
+import { useNavigate } from 'react-router-dom';
 
 
 const mainHeader = () => {
@@ -20,44 +12,49 @@ const mainHeader = () => {
     Auth.logout();
   };
 
+
+  const goToHome = (event) => {
+    event.preventDefault();
+    navigate('/Home'); // Ensure the correct route for the AdoptPet component
+  };
   const goToAdoptPet = (event) => {
     event.preventDefault();
     navigate('/AdoptPet'); // Ensure the correct route for the AdoptPet component
   };
+  const goToPetCare = (event) => {
+    event.preventDefault();
+    navigate('/PetCare'); // Ensure the correct route for the AdoptPet component
+  };
+  const goToPetStatus = (event) => {
+    event.preventDefault();
+    navigate('/PetStatus'); // Ensure the correct route for the AdoptPet component
+  };
+  const goToAchievements = (event) => {
+    event.preventDefault();
+    navigate('/Achievements'); // Ensure the correct route for the AdoptPet component
+  };
   return (
     <div>
       {Auth.loggedIn() ? (
-        <Menu color="teal" fixed="top" inverted>
+        <Menu color='teal' fixed='top' inverted>
           <Container>
-
             <Menu.Item as='a' header>
               <Icon name='paw' />
-
               Pet Adoption Application
             </Menu.Item>
-            <Menu.Item as="a">Home</Menu.Item>
-            <Dropdown item simple text="Dropdown">
+            <Menu.Item as='a' onClick={goToHome}>Home</Menu.Item>
+            <Dropdown item simple text='Dropdown'>
               <Dropdown.Menu>
-                <Dropdown.Item>
-                  <Link to="/adoptpet">Adopt a Pet</Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                  <Link to="/petcare">Pet Care</Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                  <Link to="/petstatus">Pet Status</Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                  <Link to="/achievements">Achievements</Link>
-                </Dropdown.Item>
-
+                <Dropdown.Item onClick={goToAdoptPet}>Adopt a Pet</Dropdown.Item>
+                <Dropdown.Item onClick={goToPetCare}>Pet Care</Dropdown.Item>
+                <Dropdown.Item onClick={goToPetStatus}>Pet Status</Dropdown.Item>
+                <Dropdown.Item onClick={goToAchievements}>Achievements</Dropdown.Item>
                 <Dropdown.Item onClick={logout}>Log Out</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </Container>
         </Menu>
       ) : (
-
         <Menu color='teal' fixed='top' inverted>
        <Container>
        <Menu.Item as='a' header>
@@ -68,7 +65,6 @@ const mainHeader = () => {
        </Menu>
        
        
-
       )}
     </div>
   );
