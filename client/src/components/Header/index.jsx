5bda53af5fba8e2b8b3946954b5e0114251d04fc
Ash53rd <aandersonatx@gmail.com>
2024-07-31 19:47:48 -0500

    updated navbar dropdpwn links, updated page navigation for errore, removed node modules for repo cleanup

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,14 @@
-import React , { useState } from 'react'
-import { Header, Icon, HeaderContent, Menu, Container, Dropdown } from 'semantic-ui-react'
-import Auth from '../../utils/auth';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Header,
+  Icon,
+  HeaderContent,
+  Menu,
+  Container,
+  Dropdown,
+} from "semantic-ui-react";
+import Auth from "../../utils/auth";
 
 const mainHeader = () => {
   const logout = (event) => {
@@ -10,40 +18,46 @@ const mainHeader = () => {
   return (
     <div>
       {Auth.loggedIn() ? (
-        <Menu color='teal' fixed='top' inverted>
+        <Menu color="teal" fixed="top" inverted>
           <Container>
-            <Menu.Item as='a' header>
-              <Icon name='heartbeat' />
+            <Menu.Item as="a" header>
+              <Icon name="heartbeat" />
               Pet Adoption Application
             </Menu.Item>
-            <Menu.Item as='a'>Home</Menu.Item>
-            <Dropdown item simple text='Dropdown'>
+            <Menu.Item as="a">Home</Menu.Item>
+            <Dropdown item simple text="Dropdown">
               <Dropdown.Menu>
-                <Dropdown.Item>Adopt a Pet</Dropdown.Item>
-                <Dropdown.Item>Pet Care</Dropdown.Item>
-                <Dropdown.Item>Pet Status</Dropdown.Item>
-                <Dropdown.Item>Achievements</Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/adoptpet">Adopt a Pet</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/petcare">Pet Care</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/petstatus">Pet Status</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/achievements">Achievements</Link>
+                </Dropdown.Item>
                 <Dropdown.Item onClick={logout}>Log Out</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </Container>
         </Menu>
       ) : (
-        <Menu color='teal' fixed='top' inverted>
-       <Container>
-       <Menu.Item as='a' header>
-       <Icon name='heartbeat' />
-        Pet Adoption Application
-       </Menu.Item>            
-      {/* <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        <Menu color="teal" fixed="top" inverted>
+          <Container>
+            <Menu.Item as="a" header>
+              <Icon name="heartbeat" />
+              Pet Adoption Application
+            </Menu.Item>
+            {/* <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
         {isLoggedIn ? 'Log Out' : 'Log In'}
       </button>*/}
-       </Container>
-       </Menu>
-       
-       
+          </Container>
+        </Menu>
       )}
     </div>
   );
 };
-export default mainHeader
+export default mainHeader;
